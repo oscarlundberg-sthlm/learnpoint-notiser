a711d3dbcd9d6473bf7c70b661cfb615047cfb87
oscarlundberg-sthlm <oscarlundberg@hotmail.com>
2021-11-29 19:27:11 +0100

    v2

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const loginDetails = require('./login-details.json');
 
 const readTheNews = async () => {
     try {
-        const oldNews = JSON.parse(await fs.readFile('newsfeed.json'));
-
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto('https://jenseneducation.learnpoint.se/NewsFeedForms/NewsFeed.aspx');
@@ -24,36 +22,45 @@ const readTheNews = async () => {
         const news = await page.evaluate(() => {
             return [...document.querySelectorAll('.ascx-group-posts__item')].map(post => post.innerText);
         });
+
+        const regex = /\nidag\s•/i; // /\n[string]\s•/i
+        const todaysPosts = news.filter(post => regex.test(post));
         
         // Something new?
-        if (news[0] !== oldNews[0]) {
-            console.log('New stuff on Learnpoint');
+        if (todaysPosts.length > 0) {
+            const todaysPostsLastUpdate = JSON.parse(await fs.readFile('newsfeed.json'));
 
-            //Send email if something new
-            let emailTransporter = nodemailer.createTransport({
-                host: "smtp-mail.outlook.com",
-                port: 587,
-                secure: false, // upgrade later with STARTTLS
-                auth: {
-                    user: loginDetails.email.username,
-                    pass: loginDetails.email.password,
-                },
-            });
-            let emailSend = await emailTransporter.sendMail({
-                from: `${loginDetails.email.sendername} <${loginDetails.email.emailaddress}>`,
-                to: loginDetails.email.emailaddress,
-                subject: 'Nytt på Learnpoint',
-                text: `3 senaste posterna: \n\n
-                ${news[0]}\n\n
-                ${news[1]}\n\n
-                ${news[2]}
-                `
-            })
-            console.log('email sent, messageID: ' + emailSend.messageId);
+            if (todaysPosts[0] !== todaysPostsLastUpdate[0]) {
+                let date = new Date();
+                let localDateTime = date.toLocaleString('sv-SE');
+    
+                console.log('\n\n=======================\n\n');
+                console.log(`${localDateTime}: New stuff on Learnpoint\n\n`);
+                console.log(todaysPosts.join('\n\n-------------------------\n\n'));
+        
+                //Send email if something new
+                let emailTransporter = nodemailer.createTransport({
+                    host: "smtp-mail.outlook.com",
+                    port: 587,
+                    secure: false, // upgrade later with STARTTLS
+                    auth: {
+                        user: loginDetails.email.username,
+                        pass: loginDetails.email.password,
+                    },
+                });
+                let emailSend = await emailTransporter.sendMail({
+                    from: `${loginDetails.email.sendername} <${loginDetails.email.emailaddress}>`,
+                    to: loginDetails.email.emailaddress,
+                    subject: 'Nytt på Learnpoint',
+                    text: `${todaysPosts.join('\n\n-------------------------\n\n')}`
+                })
+                console.log('email sent, messageID: ' + emailSend.messageId);
+    
+                //Update newsfeed JSON
+                await fs.writeFile('newsfeed.json', JSON.stringify(todaysPosts));
+            }
         }
 
-        //Update newsfeed JSON
-        await fs.writeFile('newsfeed.json', JSON.stringify(news));
       
         await browser.close(); 
     } catch (error) {
